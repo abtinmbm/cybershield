ec5f208c4bbae238a0506f4e03f05d99a42f3f4b
abtinmbm <abtintest@gmail.com>
2025-03-01 21:33:55 -0500

    changes

--- cybershield/urls.py
+++ cybershield/urls.py
@@ -1,11 +1,14 @@
-# ...existing code...
+from django.contrib import admin
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    # ...existing urls...
-] 
+    path('admin/', admin.site.urls),
+    path('', include('forum.urls')),
+]
 
-# Add this at the end of the file
+# Serve media files during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
